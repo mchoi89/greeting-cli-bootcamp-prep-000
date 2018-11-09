0dd3399709c2169e2c1ed8c9026b96991c6b9b46
mchoi89 <mchoi89@gmail.com>
2018-11-09 15:04:24 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,11 @@
+#!/usr/bin/env ruby
+require_relative "../lib/greeting.rb"
+
+puts "Hi! I'm HAL, what's your name?
+name = gets.strip
+
+greeting(name)
+
 def greeting(name)
   puts "Hello #{name.strip}. It's nice to meet you."
 end
